532dbabddb9c7bca04d85ebdec86285ab0cd0b8b
GlenRunc <pourcinematteo@gmail.com>
2023-11-21 22:54:28 +0100

    Fix: Optimisation run function

--- Src/game.py
+++ Src/game.py
@@ -28,39 +28,34 @@ class Game:
         self.screen.blit(pygame.image.load("../Asset/tag.png"),(225,175))
 
     def run(self):
-    
         while True:
             self.grid.win(self.screen)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-                if (event.type == pygame.MOUSEBUTTONDOWN):
-                    if(event.button == 1):
-                        (x,y) = pygame.mouse.get_pos()
-                        
-                        (x,y) = ((y-40)//45,(x-40)//45)
-                        
-                        if self.in_menu and 6<= x <= 7 and 5 <= y <= 10:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    x, y = pygame.mouse.get_pos()
+                    x, y = (y - 40) // 45, (x - 40) // 45
+
+                    if self.in_menu:
+                        if 6 <= x <= 7 and 5 <= y <= 10:
                             self.in_menu = False
-                            self.screen.fill((255,255,255))
-                
-                        if self.in_menu and 9<= x <= 10 and 5 <= y <= 10:
+                            self.screen.fill((255, 255, 255))
+                        elif 9 <= x <= 10 and 5 <= y <= 10:
                             pygame.quit()
                             sys.exit()
+                    else:
+                        self.grid.reset(x, y)
+                        self.grid.move(x, y)
 
-                        if self.in_menu == False:
-                            self.grid.reset(x,y)
-                            self.grid.move(x,y)    
-                
+            if self.in_menu:
+                self.display_menu()
+            else:
+                self.grid.display(self.screen)
 
-                if self.in_menu:
-                    self.display_menu()
-                else:
-                    self.grid.display(self.screen)
             pygame.display.flip()
             self.clock.tick(30)
-
-
 
