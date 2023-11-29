498df749bb4d080c32a1ac2988fe04515b32f007
GlenRunc <pourcinematteo@gmail.com>
2023-11-29 19:33:06 +0100

    Fix: Improvement of BFS, look at the state during the generation

--- Src/main.py
+++ Src/main.py
@@ -13,6 +13,7 @@ if __name__ == "__main__" :
     grid.possible_move()
     initial_node = Node(grid.position_robot,None)
     test_fifo = deque()
+    # print(grid.possible_move_per_robot)
     clean_all_status(grid,grid.position_robot)
 
     path = BFS(grid,initial_node,grid.color_goal,grid.goal_coordinate)
